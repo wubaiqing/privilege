ba551a8bac13fc56b0be4b9b23b4f17d2ecc06e4
wubaiqing <wubaiqing@vip.qq.com>
2014-10-13 18:02:23 +0800

    Initial Commit

---
+++ privilege/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  privilege
+//
+//  Created by 吴佰清 on 14-10-13.
+//  Copyright (c) 2014年 吴佰清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ privilege/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  privilege
+//
+//  Created by 吴佰清 on 14-10-13.
+//  Copyright (c) 2014年 吴佰清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
